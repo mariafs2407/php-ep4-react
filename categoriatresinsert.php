7ba6dfe1f287cedb1b3efd2c1a6740c543db2556
mariafs2407 <mariafs2407@gmail.com>
2022-07-07 10:52:25 -0500

    previsualizacion final

--- categoriatresinsert.php
+++ categoriatresinsert.php
@@ -5,7 +5,7 @@ require_once("config.php");
 $nombre= $_POST["nombre"];
 $descripcion=$_POST["descripcion"];
 $marcas=$_POST["marcas"];//va en apostrofes porq es string si es number no lleva
-$rs =$cn ->query("INSERT INTO categoriatres (nombre,descripcion) values ('$nombre','$descripcion','$marcas')");
+$rs =$cn ->query("INSERT INTO categoriatres (nombre,descripcion,marcas) values ('$nombre','$descripcion','$marcas')");
 echo $cn ->insert_id;//me devuelve el autonumerico generado 
 $cn ->close();
 ?>
