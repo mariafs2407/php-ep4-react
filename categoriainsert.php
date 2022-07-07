54c4b8f72ec22ecdb11cb3aa2bd8f3e258264d0b
mariafs2407 <mariafs2407@gmail.com>
2022-07-06 23:39:27 -0500

    php arreglado

--- categoriainsert.php
+++ categoriainsert.php
@@ -3,8 +3,8 @@
 require_once("config.php");
 //variables de mi tabla
 $nombre= $_POST["nombre"];
-$descrip=$_POST["descripcion"];//va en apostrofes porq es string si es number no lleva
-$rs =$cn ->query("INSERT INTO categoria (nombre,descripcion) values ('$nombre','$descrip')");
+$descripcion=$_POST["descripcion"];//va en apostrofes porq es string si es number no lleva
+$rs =$cn ->query("INSERT INTO categoria (nombre,descripcion) values ('$nombre','$descripcion')");
 echo $cn ->insert_id;//me devuelve el autonumerico generado 
 $cn ->close();
 ?>
--- categoriaupdate.php
+++ categoriaupdate.php
@@ -3,12 +3,12 @@ require_once("config.php");
 //variables de mi tabla
 $idcategoria = $_POST["idcategoria"];
 $nombre = $_POST["nombre"];
-$descrip=$_POST["descripcion"];
+$descripcion=$_POST["descripcion"];
 
 $rs = $cn->query(
     "UPDATE categoria 
     SET nombre='$nombre' ,
-    descripcion='$descrip' 
+    descripcion='$descripcion' 
     WHERE idcategoria= $idcategoria");
 $cn->close();
 
