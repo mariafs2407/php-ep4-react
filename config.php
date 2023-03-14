949c4c07d32b2315e56722ce334fc8dfe088715c
MariaF <mariafs2407@gmail.com>
2023-03-13 19:20:25 -0500

    modificando el config

--- config.php
+++ config.php
@@ -4,7 +4,6 @@ header("Access-Control-Allow-Origin: *");
 //localhost -root - contraseña -nombredelabd
 $cn = new mysqli("containers-us-west-187.railway.app","root","WXmV1nUCekSbDetn5L4P","railway");
 mysqli_set_charset($cn, "utf8")
-  
-
+    
 
 ?>
