9cf1b7f5623c6b136d24c3662bdfbba27cc61089
MariaF <mariafs2407@gmail.com>
2023-03-14 17:33:39 -0500

    modificacion de docker3

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
-//localhost -root - contraseña -nombredelabd
+//-localhost -root - contraseña -nombredelabd
 $cn = new mysqli("containers-us-west-187.railway.app","root","WXmV1nUCekSbDetn5L4P","railway");
 mysqli_set_charset($cn, "utf8")
-
+   
 
 ?>
